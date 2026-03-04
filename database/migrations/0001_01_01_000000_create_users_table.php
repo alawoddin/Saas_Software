7de949bc1466bbc11ebd7f405782f569cc757d50
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-03-04 14:00:41 +0430

    the user table is edit

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,13 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->enum('role' , ['admin' , 'user'])->default('user');
+            $table->unsignedBigInteger('plan_id')->default(1);
+            $table->integer('token_used')->default(0);
+            $table->string('status')->default('1');
             $table->rememberToken();
             $table->timestamps();
         });
